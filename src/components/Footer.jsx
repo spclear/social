221e3831e7ba3aa6be59b776c162963b46a9e492
Саша Плотников <plotnikovfw.adt@gmail.com>
2020-07-26 22:57:12 +0300

    some minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className='footer'>
-      some footer info
+      <p className='footer__info'>some footer info</p>
     </footer>
   );
 }
