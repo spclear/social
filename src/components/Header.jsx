316abfbfbb6d41cd94c76b8603cac3819a1f66f0
Саша Плотников <plotnikovfw.adt@gmail.com>
2020-07-27 13:04:47 +0300

    changed profile section styles, removed some useless elements, made colors to fit better

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ const Header = () => {
   return (
     <header className='header'>
       <a href='/' className='header__logo'></a>
+      <a href='/' className='header__title'>SociaL C.</a>
     </header>
   );
 }
