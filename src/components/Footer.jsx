d804604f2bf6a2b317d5256cccefa9db8b52a32b
Саша Плотников <plotnikovfw.adt@gmail.com>
2020-07-27 12:09:41 +0300

    back to normal state

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className='footer'>
-      <p className='footer__info'>some <span className='lul'>footer</span> info</p>
+      <p className='footer__info'>some footer info</p>
     </footer>
   );
 }
