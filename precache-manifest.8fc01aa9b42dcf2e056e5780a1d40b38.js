54849fe7f3a9c471704fdd3b731fd0fff42d06bf
seconduniQuee <plotnikovfw@gmail.com>
2020-09-08 11:46:52 +0300

    Updates

---
+++ precache-manifest.8fc01aa9b42dcf2e056e5780a1d40b38.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144166a5ae5574bca75cbadcb0f8f841",
+    "url": "/social/index.html"
+  },
+  {
+    "revision": "1fde1a63ef89ecb292a3",
+    "url": "/social/static/css/main.220e8634.chunk.css"
+  },
+  {
+    "revision": "5a618ced0a82a508f0e0",
+    "url": "/social/static/js/2.c80e5fe7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/social/static/js/2.c80e5fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fde1a63ef89ecb292a3",
+    "url": "/social/static/js/main.8f598e5d.chunk.js"
+  },
+  {
+    "revision": "245b1d1fb60e0de385d4",
+    "url": "/social/static/js/runtime-main.3c0aa0bb.js"
+  },
+  {
+    "revision": "9d1403c3ec0668184381d6490fa21a8d",
+    "url": "/social/static/media/UserDefault.9d1403c3.png"
+  },
+  {
+    "revision": "0606301cdbcd0b690d50e86719f32899",
+    "url": "/social/static/media/avatar1.0606301c.jpg"
+  },
+  {
+    "revision": "aa610c09ddfe2cd319ce0f4f627848d2",
+    "url": "/social/static/media/avatar2.aa610c09.jpg"
+  },
+  {
+    "revision": "b2eb87e5b0ef014ef3c6659081f55172",
+    "url": "/social/static/media/avatar3.b2eb87e5.jpg"
+  },
+  {
+    "revision": "13aec6a5c28f636d511f1aaac1ae6dc2",
+    "url": "/social/static/media/avatar4.13aec6a5.jpg"
+  }
+]);
